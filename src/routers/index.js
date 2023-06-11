67fd06fa17d8cc038d3481f8dff9e88efca1a7f8
Gnauqoa <quanglng2022@gmail.com>
2023-06-11 14:42:32 +0700

    ready to test router/student/auth_flow

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,7 @@
 import express from "express";
+import studentAccountRouter from "./student.js";
 
 const router = express.Router();
+router.use("/student", studentAccountRouter);
 
 export default router;
